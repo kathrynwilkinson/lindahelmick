c97a558b0dc1a2e7b8b433e3f2b81cf63a889dba
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-04-12 13:17:49 -0500

    style: Add header image options; style nav
    Remove links to pages - that will be modals included on the Philosophy page; increase font size and add slight background to visibility

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,16 @@
 import './style.css';
 import React from 'react';
 import Navigation from '../../components/Navigation';
+// import header1 from '../../images/headers/1q-color.JPG';
+// import header2 from '../../images/headers/4q-mono.JPG';
+// import header3 from '../../images/headers/angel-collage-mono.JPG';
+// import header4 from '../../images/headers/poppies-color.JPG';
 
 
 export default function Header () {
     return (
         <div>
-            <div className='header-img'>Header background-image
+            <div className='header-img'>
                 <Navigation />
             </div>
         </div>
--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -13,18 +13,18 @@ export default function Navigation () {
                 <Link to='/gallery' className='nav-link'>
                     Gallery
                 </Link>
-                <Link to='/research' className='nav-link'>
+                {/* <Link to='/research' className='nav-link'>
                     Research
-                </Link>
-                <Link to='/teaching' className='nav-link'>
+                </Link> */}
+                {/* <Link to='/teaching' className='nav-link'>
                     Teaching
-                </Link>
+                </Link> */}
                 <Link to='/philosophy' className='nav-link'>
                     Philosophy
                 </Link>
-                <Link to='/blog' className='nav-link'>
+                {/* <Link to='/blog' className='nav-link'>
                     Blog
-                </Link>
+                </Link> */}
                 <Link to='/cv' className='nav-link'>
                     CV
                 </Link>
