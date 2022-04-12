d5c290690b7664f1fbe47472f0e897b25113236d
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-04-12 11:53:26 -0500

    build + style: Add current exhibit carlousel, buttons, and links to homepage

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import './style.css';
 import React from 'react';
 import headshot from '../../images/headshot-round.png';
+import QualiaCarousel from '../../components/QualiaCarousel';
 
 export default function Home () {
     return (
         <div className='page-body' id='home-page'>
 
-            <p className='body'>
+            <p className='artist-bio'>
                 <strong>Linda Helmick </strong>
                 <em>(she, her, hers) </em>earned her Ph.D. in Curriculum and Instruction,
                 Art Education at Indiana University and currently works as an Assistant Professor
@@ -18,7 +19,26 @@ export default function Home () {
             </p>
             <img id='headshot' src={headshot} alt='artist headshot' />
 
-            <div id='current-exhibit'></div>
+            <div id='current-exhibit-container'>
+                <a href='../Gallery'><h2 id='current-exhibit-header-txt'>Current Exhibit</h2></a>
+                <QualiaCarousel />
+            </div>
+
+            <div className='main-btns-wrapper'>
+                <a className='main-btns-link' href='../Gallery'>
+                    <button className='main-btns' id='see-work-btn'>
+                        <span className='btn-txt' id='see-work'>Gallery</span>
+                        <span className='btn-txt guillemet'>&raquo;</span>
+                    </button>
+                </a>
+                <a className='main-btns-link' href='../Contact'>
+                    <button className='main-btns' id='contact-btn'>
+                        <span className='btn-txt' id='contact-me'>Contact</span>
+                        <span className='btn-txt guillemet'>&raquo;</span>
+                    </button>
+                </a>
+            </div>
+
 
         </div>
     );
